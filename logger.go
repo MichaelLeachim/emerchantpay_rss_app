f0c9efd3a585a1e2eb8a24ee97a915229617a3ca
mik <mklimoff222@gmail.com>
2019-05-19 21:57:55 +0300

    implemented logger interface

--- logger.go
+++ logger.go
@@ -7,8 +7,71 @@
 
 package main
 
+import (
+	logrus "github.com/sirupsen/logrus"
+	"testing"
+)
+
 type logger interface {
 	Warn(args ...interface{})
 	Error(args ...interface{})
 	Info(args ...interface{})
+}
+
+type mockLogger struct{}
+
+func newMockLogger() logger {
+	return mockLogger{}
+}
+
+func (m mockLogger) Error(args ...interface{}) {
+	return
+}
+func (m mockLogger) Warn(args ...interface{}) {
+	return
+}
+
+func (m mockLogger) Info(args ...interface{}) {
+
+}
+
+type testLogger struct {
+	t *testing.T
+}
+
+func newTestLogger(t *testing.T) logger {
+	return testLogger{t: t}
+}
+
+func (m testLogger) Error(args ...interface{}) {
+	m.t.Log(args...)
+	return
+}
+
+func (m testLogger) Warn(args ...interface{}) {
+	m.t.Log(args...)
+}
+func (m testLogger) Info(args ...interface{}) {
+	m.t.Log(args...)
+}
+
+type logrusLogger struct {
+}
+
+func newLogrusLogger() logger {
+	return logrusLogger{}
+}
+
+func (m logrusLogger) Error(args ...interface{}) {
+	logrus.Error(args...)
+
+}
+
+func (m logrusLogger) Warn(args ...interface{}) {
+	logrus.Warn(args...)
+}
+
+func (m logrusLogger) Info(args ...interface{}) {
+	logrus.Info(args...)
+
 }
