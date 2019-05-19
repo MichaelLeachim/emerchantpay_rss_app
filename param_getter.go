932bdc7fdb01f2a5864f482221d5724be31b22b4
mik <mklimoff222@gmail.com>
2019-05-19 22:12:52 +0300

    implemented param_getter interface

--- param_getter.go
+++ param_getter.go
@@ -6,6 +6,10 @@
 // @@@@@@ At 2019-05-19 21:59 <thereisnodotcollective@gmail.com> @@@@@@@@@@@@@@@@@@@@@@@@
 package main
 
+import (
+	"flag"
+)
+
 type paramGetter interface {
 	String(name string, value string, usage string) *string
 	Bool(name string, value bool, usage string) *bool
@@ -13,8 +17,35 @@ type paramGetter interface {
 	Parse()
 }
 
+type listOfUrls []string
+
+func (i *listOfUrls) String() string {
+	return "my string representation"
+}
+
+func (i *listOfUrls) Set(value string) error {
+	*i = append(*i, value)
+	return nil
+}
+
 type flagParamGetter struct {
 }
 
-type envParamGetter struct {
+func (f flagParamGetter) String(name string, value string, usage string) *string {
+	return flag.String(name, value, usage)
+}
+
+func (f flagParamGetter) Bool(name string, value string, usage string) *bool {
+	return flag.Bool(name, value, usage)
+}
+
+func (f flagParamGetter) List(name string, value string, usage string) []string {
+	urlset := listOfUrls{}
+	flag.Var(&urlset, "urlset", "Urls, that contain the data to parse")
+	return urlset
+}
+
+func (f flagParamGetter) Parse() {
+	flag.Parse()
+
 }
